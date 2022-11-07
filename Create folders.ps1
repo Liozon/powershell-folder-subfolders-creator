4e8be4091d1a303b1465d4324101c9ec5330a0e5
Julien Muggli <liozon@hotmail.com>
2022-11-07 15:18:49 +0100

    Improved script
    Improved script by adding the version number and cleaning up the comments

--- Create folders.ps1
+++ Create folders.ps1
@@ -1,96 +1,119 @@
-﻿# Script developped by Julien Muggli - 2022
+﻿# Script developped by Julien Muggli - 2022 - v1.1
 # In case of questions, please contact me on GitHub: https://github.com/Liozon
+$ScriptVersion = 1.1
 
-<# Clear the current windows and display credits #>
+# Clear the current windows and display credits
 Clear-Host
-Write-Host "Script developped by Julien Muggli - 2022" -ForegroundColor Cyan
+Write-Host "Script developped by Julien Muggli - 2022 - v$ScriptVersion" -ForegroundColor Cyan
 Write-Host
 Write-Host "In case of questions, please contact me on GitHub: https://github.com/Liozon" -ForegroundColor Cyan
 Write-Host " "
 
-# Import FoldersNames.csv into a variable called $Folders
-# This will create an array of objects which have the Name and Type properties as defined in the CSV.
-$Folders = Import-Csv FolderNames.csv -Delimiter ';';
-$Folderscount = $Folders | Where-Object { $_.Type -eq 'Folder' };
-$Subfolderscount = $Folders | Where-Object { $_.Type -eq 'Subfolder' };
+# Import the CSV file into a variable
+$Folders = Import-Csv FolderNames.csv -Delimiter ';'
+
+# Count how many folders and subfolders are declared in file
+$Folderscount = $Folders | Where-Object { $_.Type -eq 'Folder' }
+$Subfolderscount = $Folders | Where-Object { $_.Type -eq 'Subfolder' }
 
 # Promt to warn user prior to start script
-## The following four lines only need to be declared once in your script.
+# Call to action
 $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "The script will run and create $($Folderscount.length) folders and $($Subfolderscount.length) subfolders in the current directory."
 $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "No file creation will be carried out will be stopped."
 $cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel", "No file creation will be carried out will be stopped."
 $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no, $cancel)
 
-## Use the following each time your want to prompt the use
-$title = "Creating folders and subfolders";
+# Prompt message
+$title = "Creating folders and subfolders"
 $message = @"
 WARNING: this script will create $($Folderscount.length) folders and $($Subfolderscount.length) subfolders.
 If folders or files already exist, they may be overwritten or the script will not run.
 Would you like to run the file creation script ?
-"@;
+"@
 
-
+# Beginning of the script
 function startScript() {
     $result = $host.ui.PromptForChoice($title, $message, $options, 1)
 
     switch ($result) {
         0 {
+            # Feedback in console
             Write-Host "Starting script..."
+
+            # Call function
             createFolders
         }
         1 {
+            # Feedback in console
             Write-Host "Operation cancelled"
+
+            # Call function
             closeScript
         }
         2 {
+            # Feedback in console
             Write-Host "Operation cancelled"
+
+            # Call function
             closeScript
         }
     }
 }
 
+# Function to create the folders, based on data in CSV file
 function createFolders() {
     for ($i = 0; $i -le ($Folders.length - 1); $i++) {
-        #Write-Host $Folders[$i].Name
+
+        # Check if the line is a 'Folder' type
         if ($Folders[$i].Type -eq "Folder") {
             
             # Store the current object's Name in $ParentName.
-            $ParentName = $Folders[$i].Name;
+            $ParentName = $Folders[$i].Name
     
             # Create a new directory named $ParentName.
-            New-Item "$ParentName" -ItemType Directory;
-            <# Set-Location . #>
+            New-Item "$ParentName" -ItemType Directory
     
-            Write-Host "Folder $ParentName created";
+            Write-Host "Folder $ParentName created"
         }
+        # Check if the line is a 'Subfolder' type
         elseif ($Folders[$i].Type -eq "Subfolder") {
             
             # Store the current object's Name in $ChildName.
-            $ChildName = $Folders[$i].Name;
+            $ChildName = $Folders[$i].Name
     
             # Create a new subfolder in $ParentName called $ChildName.
-            Set-Location -Path .\$ParentName;
+            Set-Location -Path .\$ParentName
             New-Item "$ChildName" -ItemType Directory
     
-            Write-Host "Subfolder $ChildName in folder $ParentName created";
+            # Feedback in console
+            Write-Host "Subfolder $ChildName in folder $ParentName created"
+
+            # Go to the folder on upper level
             Set-Location ..
         }
         else {
-            Write-Host "An error occurred while reading the CSV file...." -ForegroundColor Red
+            # Feedback in console with the error line
+            Write-Host "An error occurred while reading the CSV file on line $($i+2)...." -ForegroundColor Red
         }
     }
+
+    # CLose the script after loop
     closeScript
 }
 
-<# Create a progress bar to whow the remaining time before closing windows #>
+# Create a progress bar to whow the remaining time before closing windows
 function closeScript() {
+    # Feedback in console
     Write-Host "This window will close automatically in 10 seconds..." -ForegroundColor Green
+
+    # Visual loop to create the countown in console
     $seconds = 10
     1..$seconds |
-    ForEach-Object { $percent = $_ * 100 / $seconds; 
-        Write-Progress -Activity "Closing" -Status "Closing window in $($seconds - $_) seconds..." -PercentComplete $percent;
+    ForEach-Object { $percent = $_ * 100 / $seconds
+        Write-Progress -Activity "Closing" -Status "Closing window in $($seconds - $_) seconds..." -PercentComplete $percent
         Start-Sleep -Seconds 1
     }
 }
 
+# Script initiator
 startScript
